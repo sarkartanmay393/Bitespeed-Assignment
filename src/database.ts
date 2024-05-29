33c4bb28cbfafde8ee95f6d64c0644b09e86bdc6
Tanmay Sarkar <sarkartanmay393@gmail.com>
2024-05-29 06:27:05 +0530

    feat: created database connection object using sequalize

---
+++ src/database.ts
@@ -0,0 +1,18 @@
+import fs from "fs";
+import { Sequelize } from "sequelize";
+
+export const sequelize = new Sequelize({
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: "bitespeed",
+  host: "pg-1d02b618-sarkartanmay393-034b.a.aivencloud.com",
+  port: 14056,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true,
+      ca: fs.readFileSync(__dirname + "/../ca.pem").toString(),
+    },
+  },
+});
